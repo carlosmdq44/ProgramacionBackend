e9dbdd80258e6780fe29100deacae026ad54999e
Carlos J Figueroa <71535916+carlosmdq44@users.noreply.github.com>
2022-03-20 19:04:12 -0300

    Add files via upload

---
+++ Entrega3/controllers/index.js
@@ -0,0 +1,9 @@
+
+const obtenerProductos =  () => {
+    return archi1.getAll();
+}
+
+const obtenerProductosRandom = () => {return "productos"}
+
+
+module.exports = {obtenerProductos,obtenerProductosRandom};
